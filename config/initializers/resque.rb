51de603faeaa420a8b49d111ad5f6ea6d98bbc0a
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-26 16:43:38 -0600

    Resque fiddling 3

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,11 @@
 require 'resque/scheduler'
 require 'resque/scheduler/server'
 
-Resque.schedule = YAML.load_file(File.join(Rails.root, './config/scheduler.yml'))
-
 if ENV["REDIS_URL"].present?
   uri = URI.parse(ENV["REDIS_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   Resque.redis = Redis.new
 end
+
+Resque.schedule = YAML.load_file(File.join(Rails.root, './config/scheduler.yml'))
