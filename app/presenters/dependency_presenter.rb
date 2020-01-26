f0204f62b220765c11dc168880b6eab7cf466436
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-26 13:39:59 -0600

    Show license in UI

--- app/presenters/dependency_presenter.rb
+++ app/presenters/dependency_presenter.rb
@@ -1,11 +1,12 @@
 class DependencyPresenter
 
-  attr_reader :name, :language, :projects, :url, :weight
+  attr_reader :name, :language, :license, :projects, :url, :weight
 
   def initialize(dependency)
     @name = dependency.name
     @projects = dependency.projects
     @language = dependency.language
+    @license = dependency.license&.name
     @weight = @projects.size
     @url = dependency.source_repo_url
   end
