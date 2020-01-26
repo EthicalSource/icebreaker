ce1ee6a5c837938caf7870c7b598f5250048b32e
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-26 16:40:36 -0600

    Resque fiddling 1

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,5 +5,5 @@ if ENV["REDIS_URL"].present?
   uri = URI.parse(ENV["REDIS_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
-  Resque.redis = Redis.new
+#  Resque.redis = Redis.new
 end
