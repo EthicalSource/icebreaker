39755191e0ca4ac74732c5b34c2efb2c06119c4e
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-26 17:01:21 -0600

    Resque fiddling 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require "resque_web"
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root "projects#index"
+
+  mount ResqueWeb::Engine => "/resque_web"
+
 end
