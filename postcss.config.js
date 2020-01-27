cd58636b8eddc3f48afd1c23778ae99f54062e1f
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-26 22:23:35 -0600

    Trying to make tailwind work 3

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    require('postcss-import'),
     require('tailwindcss')('./app/javascript/css/tailwind.js'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
   ]
 }
