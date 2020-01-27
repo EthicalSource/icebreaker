85bb30dfff8e0320d29bc7f30a86a701c09a2f4d
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-26 23:06:56 -0600

    Adjust scheduler

--- app/jobs/dependencies_fetcher_job.rb
+++ app/jobs/dependencies_fetcher_job.rb
@@ -3,10 +3,6 @@ class DependenciesFetcherJob
   @queue = :dependencies
 
   def self.perform(project_id:)
-    new.perform(project_id: project_id)
-  end
-
-  def perform(project_id:)
     project = Project.find(project_id)
     DependencyFetcher.get_dependencies(project, project.organization)
   end
