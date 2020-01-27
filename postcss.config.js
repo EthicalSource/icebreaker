296a493381b76a4903c68bdb3e3f1ccd9000e7fb
Zee <zee@zincma.de>
2020-01-27 12:38:46 -0800

    Fix tailwind compilation on deploy to heroku

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-// module.exports = {
-//   plugins: [
-//     require('tailwindcss')('./app/javascript/css/tailwind.js'),
-//     require('autoprefixer'),
-//   ]
-// }
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-import'),
+    require('tailwindcss')('./app/javascript/src/tailwind.config.js'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+    }),
+  ],
+};
