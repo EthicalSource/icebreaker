0aad828f90a0681da838f4f31b82c98bc816a9e6
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-27 20:59:52 -0600

    Unique projects in bricks

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ApplicationRecord
   has_many :dependency_instances
   has_many :dependencies, through: :dependency_instances
 
+  validates :name, uniqueness: true, presence: true
+
   def github_identifier
     "#{self.org_name}/#{self.name}"
   end
