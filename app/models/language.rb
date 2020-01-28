15d61d83e07f5453e135ed790b678fb2db40b75a
Coraline Ada Ehmke <coraline@idolhands.com>
2020-01-27 20:48:30 -0600

    UI 2

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,3 @@
 class Language < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
 end
--- app/presenters/dependency_presenter.rb
+++ app/presenters/dependency_presenter.rb
@@ -6,15 +6,8 @@ class DependencyPresenter
     @name = dependency.name
     @projects = dependency.projects
     @language = dependency.language
-    @license = license_name(dependency.license&.name)
     @weight = @projects.size
     @url = dependency.source_repo_url
-  end
-
-  def license_name(name)
-    return if name.nil? || name.empty?
-    return if name == "Unknown"
-    "#{name} license"
   end
 
 end
