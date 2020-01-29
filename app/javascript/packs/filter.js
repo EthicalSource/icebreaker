51e040432fb5a78ea68561a75b2e3f931924e9da
Dean <deangalvin3@gmail.com>
2020-01-28 20:03:44 -0500

    Added color and pointer to filter

--- app/javascript/packs/filter.js
+++ app/javascript/packs/filter.js
@@ -1,4 +1,6 @@
 window.filter = function(filterBy) {
+  $(".selected").removeClass("selected");
+  $("#filter-"+filterBy).addClass("selected");
   var all = $(".dependency")
   var show = $("." + filterBy)
   all.hide()
